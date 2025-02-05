56b18aaaefa78755b52a994a0ca34c1aed87fc5d
JhayStark <jaamen5@gmail.com>
2025-02-05 08:11:28 +0000

    chore

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(morgan('dev'));
 app.use(credentials);
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 
 const port = process.env.PORT || 3001;
